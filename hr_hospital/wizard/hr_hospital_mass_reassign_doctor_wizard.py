d0fcba6fbd15e04a77c09e24e3583a205e921eeb
gavrilenko_an <havrilenko.ann@gmail.com>
2026-07-07 16:21:34 +0300

    feat: support scheduled doctor reassignment in wizard

--- hr_hospital/wizard/hr_hospital_mass_reassign_doctor_wizard.py
+++ hr_hospital/wizard/hr_hospital_mass_reassign_doctor_wizard.py
@@ -16,12 +16,29 @@ class MassReassignDoctorWizard(models.TransientModel):
     )
 
     def action_reassign(self):
+        self.ensure_one()
+
         patients = self.env['hospital.patient'].browse(self.env.context.get('active_ids', []))
 
-        patients.write(
-            {
+        history_model = self.env['hospital.doctor.history']
+
+        for patient in patients:
+            current = history_model._get_current_history(patient)
+
+            if current:
+                current.write({
+                    'change_date': self.change_date,
+                })
+
+            history_model.create({
+                'patient_id': patient.id,
                 'doctor_id': self.doctor_id.id,
-            }
-        )
+                'assignment_date': self.change_date
+            })
+
+            if self.change_date <= fields.Date.today():
+                patient.with_context(skip_history_sync=True).write({
+                    'doctor_id': self.doctor_id.id,
+                })
 
         return {'type': 'ir.actions.act_window_close'}
